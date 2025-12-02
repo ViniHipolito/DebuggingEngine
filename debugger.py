d73654de5540657d13ed8d361901537819381998
ViniHipolito <97119997+ViniHipolito@users.noreply.github.com>
2025-12-02 17:36:58 -0300

    Update debugger.py

--- debugger.py
+++ debugger.py
@@ -1,5 +1,11 @@
-["???: Outro video jogo, não?", "???: Vós conhece o primeiro.", "???: Não é um erro acidental esta linha de código.",
+import time
+
+theeList = ["???: Outro video jogo, não?", "???: Vós conhece o primeiro.", "???: Não é um erro acidental esta linha de código.",
 "???: Vós tem um raciocínio... magnifico", "???: Quem eres eu?", "???: Um Humano, apenas.", 
 "???: Este Bispo? Não há tempo de tela o bastante.", '???: Atrás de um simples jogo de "Enigma".'
 "???: Odeio ter de abrir os olhos de uns.","???: Mas... a Sheller está esperando vós.",
 "Zacharie: Também estárei."]
+
+for x in theeList:
+  print(x)
+  time.sleep(1)
